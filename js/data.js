910f90dd7c91f07245c1be3eab97d921e643e420
Rohit sahu <107259885+rohitsahu9820@users.noreply.github.com>
2026-01-11 17:01:15 +0530

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,23 @@
+const products = [
+  {
+    id: 1,
+    name: "Luxury Leather iPhone Case",
+    price: 1999,
+    image: "https://images.unsplash.com/photo-1603899123950-2b4c5fcb93f1",
+    desc: "Handcrafted premium leather with shock protection"
+  },
+  {
+    id: 2,
+    name: "Matte Silicone Case",
+    price: 1299,
+    image: "https://images.unsplash.com/photo-1580910051074-7c98c6fce6c5",
+    desc: "Soft-touch silicone with minimalist design"
+  },
+  {
+    id: 3,
+    name: "Clear MagSafe Case",
+    price: 1499,
+    image: "https://images.unsplash.com/photo-1611078489935-0cb964de46d6",
+    desc: "Crystal clear case with MagSafe support"
+  }
+];
